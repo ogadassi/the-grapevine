11ce42a48beff9ffee110f3ea4ce7e3434b0f015
ogadassi <ogadassi@gmail.com>
2024-05-28 10:57:27 +0300

    second commit

--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -12,24 +12,22 @@ function Routing(): JSX.Element {
     <div className="Routing">
       <Routes>
         {/* Home: */}
-        <Route path="/the-grapevine/home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
 
         {/* List: */}
-        <Route path="/the-grapevine/game-of-hukiyoot" element={<List />} />
+        <Route path="/game-of-hukiyoot" element={<List />} />
 
         {/* Filling Form: */}
-        <Route path="/the-grapevine/land-city" element={<Add />} />
+        <Route path="/land-city" element={<Add />} />
 
         {/* Grading Form: */}
-        <Route path="/the-grapevine/land-city/grading" element={<GradingForm />} />
+        <Route path="/land-city/grading" element={<GradingForm />} />
 
         {/* Viewing Form: */}
-        <Route path="/the-grapevine/land-city/viewing" element={<ViewingForm />} />
+        <Route path="/land-city/viewing" element={<ViewingForm />} />
 
         {/* Default Route: */}
-        <Route path="/the-grapevine/" element={<Navigate to="/the-grapevine/home" />} />
-        {/* Default Route: */}
-        <Route path="/" element={<Navigate to="/the-grapevine/home" />} />
+        <Route path="/" element={<Navigate to="/home" />} />
 
         {/* Page not found route: */}
         <Route path="*" element={<Page404 />} />
