df98bd87f38ee4b2852b2b9bcfa9412fca37051b
ogadassi <ogadassi@gmail.com>
2024-05-31 14:07:41 +0300

    third commit

--- src/Components/DataArea/List/List.tsx
+++ src/Components/DataArea/List/List.tsx
@@ -5,11 +5,13 @@ import { AppState } from "../../../Redux/AppState";
 import { useNavigate } from "react-router-dom";
 
 function List(): JSX.Element {
+  let clicks = 0;
   const [ruleString, setRuleString] = useState<string>();
 
   function getTrigger(): string {
     const triggers = [
       "השואל מציין מאכל",
+      "השואל מציין מקצוע",
       "נשאלת שאלה",
       "נשאלת שאלה עם האות {letter}",
       "השואל משנה תנוחה",
@@ -21,11 +23,14 @@ function List(): JSX.Element {
   function getPlayer(): string {
     const players = [
       "הנשאל",
+      "הנשאל הקודם",
       "האדם הבא בסבב",
       "האדם מימין לנשאל",
       "האדם משמאל לנשאל",
       "האדם ש{player} מסתכל עליו",
-
+      "{player} ו{player}",
+    ];
+    const singularPlayers = [
       "עומרי",
       "רז",
       "אביאני",
@@ -37,24 +42,49 @@ function List(): JSX.Element {
       "רון",
       "קשפיץ",
       "גדסי",
-      "{player} ו{player}",
     ];
-    return players[Math.floor(Math.random() * players.length)];
+    if (Math.random() < 0.9)
+      return players[Math.floor(Math.random() * players.length)];
+    else
+      return singularPlayers[
+        Math.floor(Math.random() * singularPlayers.length)
+      ];
   }
   function getAction(): string {
     const actions = [
+      "לענות",
+      "להתעצבן",
+      "להתמתח",
+      "לסדר את הבגדים",
+      "לענות כאילו הייתה שגיאת חוקיות",
       "לענות עם מספר המילים שהיו בשאלה",
+      "לפהק",
+      "להירדם עד שקוראים לו",
+      "לשנות תנוחה",
+      "לענות על השאלה הקודמת",
+      "לענות בתור ניצול השואה הארמנית",
+      "לענות בתור אדם נשוי",
+      "לקבל אישיות של חייזר בעל אמנזיה שמנסה להשתלב כל שניתן",
+
+      "לענות בתור ההפך המוחלט של {player}",
       "לענות כאילו {player} הביא לו ג'וינט",
-      "לקבל אישיות של חייזר שלא יודע איך הוא הגיע לנקודה הזו ומנסה להשתלב כל שניתן",
-      "לענות עם האות {letter} במילה ה{number} בתשובה",
+      "להסתכל על {player}",
+
       "לענות כשמספר המילים בתשובה הוא {number}",
       "לענות כשמספר ההברות בתשובה הוא {number}",
       "לענות כשמספר האותיות בתשובה הוא {number}",
+      "לענות עם האות {letter} במילה ה{number} בתשובה",
+      "לענות ללא האות {letter}",
+      "לענות עם המילה הבאה בארץ עיר ע''פ האות {letter}",
+
+      "{action}, יש זיקוקים מידי פעם",
+      "{action}, יש יריות מידי פעם",
       "{action}, בצעקות",
+      "{action}, בשירה",
       "{action}, בלחישות",
       "{action}, בעברית תנ''כית",
       "{action}, כאילו אנחנו במאה ה-1{number}",
-      "לענות ללא האות {letter}",
+      "{action}, כאילו אנחנו אנשי מערות",
       "אסור {action}",
     ];
     return actions[Math.floor(Math.random() * actions.length)];
@@ -67,36 +97,40 @@ function List(): JSX.Element {
 
   function getLetter(): string {
     const hebrewAlphabet = [
-      "א",
-      "ב",
-      "ג",
-      "ד",
-      "ה",
-      "ו",
-      "ז",
-      "ח",
-      "ט",
-      "י",
-      "כ",
-      "ל",
-      "מ",
-      "נ",
-      "ס",
-      "ע",
-      "פ",
-      "צ",
-      "ק",
-      "ר",
-      "ש",
-      "ת",
+      '"א"',
+      '"ב"',
+      '"ג"',
+      '"ד"',
+      '"ה"',
+      '"ו"',
+      '"ז"',
+      '"ח"',
+      '"ט"',
+      '"י"',
+      '"כ"',
+      '"ל"',
+      '"מ"',
+      '"נ"',
+      '"ס"',
+      '"ע"',
+      '"פ"',
+      '"צ"',
+      '"ק"',
+      '"ר"',
+      '"ש"',
+      '"ת"',
+      " הראשונה בשאלה",
+      " האחרונה בשאלה",
+      " האחרונה בשם של {player}",
     ];
-    return `'${
+    return `${
       hebrewAlphabet[Math.floor(Math.random() * hebrewAlphabet.length)]
-    }'`;
+    }`;
   }
 
   function generateRule() {
     setRuleString(resolveRule(` כל פעם ש{trigger}, על {player} {action}.`));
+    // limitClicks();
   }
 
   function resolveRule(ruleStr: string): string {
@@ -115,8 +149,14 @@ function List(): JSX.Element {
     for (const symbol in symbols) {
       if (ruleStr.includes(symbol)) return resolveRule(ruleStr);
     }
+
     return ruleStr;
   }
+
+  //   function limitClicks() {
+  //     clicks++;
+  // if (clicks===3)
+  // }
 
   const navigate = useNavigate();
   const auth = useSelector<AppState>((AppState) => AppState.auth);
